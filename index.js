a3da69da4f91f60f9bee7dc0fee98a1da19ecc98
BanqiaoBasketball <117630736+BanqiaoBasketball@users.noreply.github.com>
2023-03-02 16:21:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let sendButton = document.querySelector('button');
 function send() {
   let name = document.querySelector('#nameValue').value;
   $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbxR3xBWFtM8rV-BzNzeRG-lapGz7oAZZDtEW0j6lf72XAcpWJk6e0yjpStuiZtWcxaCGA/exec",
-    data: {
+    url: "https://script.google.com/macros/s/AKfycbxYENEi4aSz-jeQtjCX2DoLd_iungMreo0JSB5R2d4O1-redl2i9jiwQZLHoMZSVySDsQ/exec",
+   data: {
         "name": name
     },
     success: function(response) {
@@ -13,7 +13,24 @@ function send() {
     },
   });
 };
+
+function resetAll() {
+	alert("ttt");
+  let name = document.querySelector('#nameValue').value;
+  $.ajax({
+    url: "https://script.google.com/macros/s/AKfycbxp14AIG00wiwNl-TuDYGKBw6_hRhkNCCePN1jFqggUgy2bBdr_LO62Ycm-eBCLSdMGFw/exec?func=reset",
+   data: {
+        "name": name
+    },
+    success: function(response) {
+        alert(response);
+      
+    },
+  });
+};
+
 function closeSystem() {
  alert('系統維護中'); 
-};
+}
 sendButton.addEventListener('click', closeSystem);
+
