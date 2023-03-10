d9f446eda2a75566d99c8a688db42651efdfabef
BanqiaoBasketball <117630736+BanqiaoBasketball@users.noreply.github.com>
2023-03-10 09:53:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 let sendButton = document.querySelector('button');
 
 function send() {
-  let name = document.querySelector('#nameValue').value;
-  $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbwUPf4abNLkZtFtsOrvpJ6E6TwHBmquXKAslleCqA_2g2064yyLCiK7CUAbiOVjBjY_AQ/exec",
-   data: {
-        "name": name
-    },
-    success: function(response) {
-        alert(response);
-      
-    },
-  });
+//  let name = document.querySelector('#nameValue').value;
+//  $.ajax({
+//    url: "https://script.google.com/macros/s/AKfycbwUPf4abNLkZtFtsOrvpJ6E6TwHBmquXKAslleCqA_2g2064yyLCiK7CUAbiOVjBjY_AQ/exec",
+//   data: {
+//        "name": name
+//    },
+//    success: function(response) {
+//        alert(response);
+     
+//    },
+//  });
+  alert('系統維護中');
 };
 
 function resetAll() {
