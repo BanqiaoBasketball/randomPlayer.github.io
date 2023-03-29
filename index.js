d2cbcb95b2a0e34408ea5ef6774ee40a68f1e737
BanqiaoBasketball <117630736+BanqiaoBasketball@users.noreply.github.com>
2023-03-29 17:47:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 let sendButton = document.querySelector('button');
 
 function send() {
- let name = document.querySelector('#nameValue').value;
- $.ajax({
-   url: "https://script.google.com/macros/s/AKfycbwUPf4abNLkZtFtsOrvpJ6E6TwHBmquXKAslleCqA_2g2064yyLCiK7CUAbiOVjBjY_AQ/exec",
-  data: {
-       "name": name
-   },
-   success: function(response) {
-       alert(response);
-     
-   },
- });
-  //alert('系統維護中');
-};
-
-function resetAll() {
   let name = document.querySelector('#nameValue').value;
   $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbwUPf4abNLkZtFtsOrvpJ6E6TwHBmquXKAslleCqA_2g2064yyLCiK7CUAbiOVjBjY_AQ/exec?func=reset",
+    url: "https://script.google.com/macros/s/AKfycbwUPf4abNLkZtFtsOrvpJ6E6TwHBmquXKAslleCqA_2g2064yyLCiK7CUAbiOVjBjY_AQ/exec",
    data: {
         "name": name
     },
@@ -28,3 +13,25 @@ function resetAll() {
     },
   });
 };
+
+function queryForHtml() {
+	document.getElementById("queryId").innerText = "查詢中..";
+  let name = document.querySelector('#nameValue').value;
+  $.ajax({
+    url: "https://script.google.com/macros/s/AKfycbx0pXgCaffVfCoFoiNWlyqALZPW34hKkiNKduQ4r0BJ00HyGUlXDN8nhyc62O7GuDsd4w/exec?func=queryForHtml",
+   data: {
+        "name": name,
+		"func":"queryForHtml"
+    },
+    success: function(response) {
+			document.getElementById("queryId").innerText =response
+
+        //alert(response);
+      
+    },
+  });
+};
+
+
+
+
