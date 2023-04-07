159c2f66c3e5b5b4fbdcf4d97fdf2ff2a34b6b3e
BanqiaoBasketball <117630736+BanqiaoBasketball@users.noreply.github.com>
2023-04-07 09:59:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let sendButton = document.querySelector('button');
 function send() {
   let name = document.querySelector('#nameValue').value;
   $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbwsbHkChWvgcfS3VKEDYmjFdcefvWFrDuIRMV1BarZJ1uvNiANK0ywxlGQp0p4grdtuow/exec",
+    url: "https://script.google.com/macros/s/AKfycbwxpVqGy0sCqb3hC6aCYNpcpKqA2Xs6Fp94AFIADvlcQ89mY1XsAc9EnPdOztO-Ik2ukg/exec",
    data: {
         "name": name
     },
