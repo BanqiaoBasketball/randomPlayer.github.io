b4bd9ae9138bd173a5b99955be21c4340d69cb3e
BanqiaoBasketball <117630736+BanqiaoBasketball@users.noreply.github.com>
2023-04-07 10:09:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let sendButton = document.querySelector('button');
 function send() {
   let name = document.querySelector('#nameValue').value;
   $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbyJjvyplbztmzMFS33ih7HsQdX-mf7i6n6Dql5JCE5f7UL4yRf4x61F0htgu7IgQPoCbg/exec",
+    url: "https://script.google.com/macros/s/AKfycbziFcUDWao6ASdwnCY12JmTOcOoUnoOtkZc1GJSdQzx0biNQPsjUzbYAvbIM8ZJbtPXdw/exec",
    data: {
         "name": name
     },
@@ -18,7 +18,7 @@ function queryForHtml() {
 	document.getElementById("queryId").innerText = "查詢中..";
   let name = document.querySelector('#nameValue').value;
   $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbwsbHkChWvgcfS3VKEDYmjFdcefvWFrDuIRMV1BarZJ1uvNiANK0ywxlGQp0p4grdtuow/exec?func=queryForHtml",
+    url: "https://script.google.com/macros/s/AKfycbziFcUDWao6ASdwnCY12JmTOcOoUnoOtkZc1GJSdQzx0biNQPsjUzbYAvbIM8ZJbtPXdw/exec?func=queryForHtml",
    data: {
         "name": name,
 		"func":"queryForHtml"
